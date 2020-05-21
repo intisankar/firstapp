f98871b417c796157de882ed6960acb00cfe79c8
sankar inti <sankar@billez.in>
2020-05-21 12:30:28 +0530

    main page

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
     path('auth/',include('authenticate.urls'))
 ]
