4b2c317e52530aa9c7d99e2c3a1a6a0e3810b06c
sankar inti <sankar@billez.in>
2020-05-21 11:57:51 +0530

    todo remove

--- todo/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from .models import sample
-
-# Create your views here.
-def index(request):
-	todo_items = sample.objects.order_by('id')
-	context = {'todo_items':todo_items}
-	return render(request,'todo/sample.html',context)
